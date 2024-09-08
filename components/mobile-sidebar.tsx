37ce57b54c9690632c37cd742e26ff558fd01573
Ye <phx13ye@gmail.com>
2024-09-08 23:33:13 +0800

    create sidebar for PC and Mobile

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,7 +3,9 @@ import { Menu } from "lucide-react";
 import {
     Sheet,
     SheetContent,
-    SheetTrigger
+    SheetTrigger,
+    SheetTitle,
+    SheetDescription
 } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
 
@@ -13,7 +15,8 @@ export const MobileSidebar = () => {
             <SheetTrigger>
                 <Menu className="text-white" />
             </SheetTrigger>
-            <SheetContent className="p-0 z-[100]" side="left">
+            
+            <SheetContent className="p-0 z-[51]" side="left">
                 <Sidebar />
             </SheetContent>
         </Sheet>
