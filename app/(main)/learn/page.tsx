9d3219f16f48f61c1b9d14a6d43fba1d600c91c4
Ye <phx13ye@gmail.com>
2024-09-11 21:10:46 +0800

    add user progress and its components

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,22 +1,35 @@
+import { redirect } from "next/navigation";
+
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
 
 import { Header } from "./header";
 
-const LearnPage = () => {
+const LearnPage = async () => {
+  const userProgressData = getUserProgress();
+
+  const [userProgress] = await Promise.all([userProgressData]);
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+          activeCourse={{
+            title: userProgress.activeCourse.title,
+            imageSrc: userProgress.activeCourse.imageSrc,
+          }}
           hearts={5}
           points={100}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="spanish" />
+        <Header title={userProgress.activeCourse.title} />
       </FeedWrapper>
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <ClerkProvider afterSignOutUrl="/">
       <html lang="en">
-        <body className={`${font.className} antialiased`}>{children}</body>
+        <body className={`${font.className} antialiased`}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
